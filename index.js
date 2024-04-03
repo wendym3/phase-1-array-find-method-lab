679200ec5b4af4701a00c8d07328dc4828ce69b7
wendymasese <wendy.masese@student.moringaschool.com>
2024-04-03 20:19:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    let yearWon = "2015"
+    return yearWon 
+};
+
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+    return win ? win.year : undefined;
+}
